3a891e275ee5244197c0fc9e6d7ae3b872c1a032
Himadri Sekhar Basu <41947504+hsbasu@users.noreply.github.com>
2023-12-29 10:45:49 +0530

    Prepare translations (#59)
    - Add all files to be translated in POTFILES
    - Add .pot file
    - Add .pot update script

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-import glob, os
-from subprocess import check_output
+import glob
+import os
+import pathlib
+import subprocess
 
 from setuptools import setup
 from distutils.log import info
 import distutils.command.install_data
 
-for line in check_output('dpkg-parsechangelog --format rfc822'.split(),
+for line in subprocess.check_output('dpkg-parsechangelog --format rfc822'.split(),
                          universal_newlines=True).splitlines():
     header, colon, value = line.lower().partition(':')
     if header == 'version':
@@ -33,9 +35,24 @@ class install_data(distutils.command.install_data.install_data):
         gschema_dir = os.path.join(self.install_dir, gschema_dir_suffix)
         self.spawn(["glib-compile-schemas", gschema_dir])
 
+PO_FILES = 'po/simple-pwgen.po'
+
+def create_mo_files():
+	mo_files = []
+	prefix = 'simple-pwgen'
+	
+	for po_path in glob.glob(str(pathlib.Path(prefix) / PO_FILES)):
+		mo = pathlib.Path(po_path.replace('.po', '.mo'))
+		
+		subprocess.run(['msgfmt', '-o', str(mo), po_path], check=True)
+		mo_files.append(str(mo.relative_to(prefix)))
+	
+	return mo_files
+
 setup(data_files=[('share/applications', glob.glob("data/applications/*.desktop")),
-            ('share/icons/hicolor/scalable/apps', glob.glob("data/icons/*")),
-            (gschema_dir_suffix, glob.glob("data/*.xml"))
-            ],
-            cmdclass = {'install_data': install_data}
+			('share/icons/hicolor/scalable/apps', glob.glob("data/icons/*")),
+			(gschema_dir_suffix, glob.glob("data/schema/*.xml")),
+			('share/locale', create_mo_files())
+			],
+			cmdclass = {'install_data': install_data}
 )
