39249bb9468ad8778338272500bbcba2bca0c9b4
Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
2024-02-15 19:03:37 +0530

    Update python build-script to get version

--- setup.py
+++ setup.py
@@ -6,16 +6,13 @@ from setuptools import setup
 from distutils.log import info
 import distutils.command.install_data
 
-for line in subprocess.check_output('dpkg-parsechangelog --format rfc822'.split(),
-						 universal_newlines=True).splitlines():
-	header, colon, value = line.lower().partition(':')
-	if header == 'version':
-		version = value.strip()
-		break
-else:
-	raise RuntimeError('No version found in debian/changelog')
-
 with open("src/SimplePwgen/VERSION", "w") as f:
+	try:
+		version = (subprocess.check_output(["dpkg-parsechangelog",
+									   "--show-field", "Version"])).decode().strip("\n")
+	except:
+		raise RuntimeError('No version found in debian/changelog')
+	
 	if '~' in version:
 		version = version.split('~')[0]
 	f.write("%s" % version)
@@ -50,7 +47,7 @@ def create_mo_files():
 		os.makedirs("data/locale/%s" % lang, exist_ok=True)
 		mo = "data/locale/%s/%s.mo" % (lang, prefix)
 		subprocess.run(['msgfmt', '-o', str(mo), po_file], check=True)
-	
+		
 		mo_file = map(lambda i: ('share/locale/%s/LC_MESSAGES' % lang, [i+'/%s.mo' % prefix]), glob.glob('data/locale/%s' % lang))
 		data_files.extend(mo_file)
 
